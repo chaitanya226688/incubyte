58fed23393b7804b5268dd1c7650bbad3d9ade10
Venkata Chaithanya <chaitanya.evolutyz@gmail.com>
2025-02-22 14:10:50 +0530

    Test Case 2

--- src/Add.test.js
+++ src/Add.test.js
@@ -3,3 +3,7 @@ import Add from "./Add";
 test("return 0 for empty string", () => {
     expect(Add("")).toBe(0);
 });
+
+test("returns the number itself if only one number is provided", () => {
+    expect(Add("1")).toBe(1);
+});
