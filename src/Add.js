edf0077e9bae25884431fc57bcacf9dbbcfd654a
Venkata Chaithanya <chaitanya.evolutyz@gmail.com>
2025-02-22 14:24:54 +0530

    Test case 5 passed

--- src/Add.js
+++ src/Add.js
@@ -3,6 +3,12 @@ const Add = (numbers) => {
 
     let splitQuote = /,|\n/
 
+    if (numbers.startsWith("//")) {
+        const items = numbers.split("\n");
+        splitQuote = new RegExp(items[0][2])
+        numbers = items[1]
+    }
+
     let nums = numbers.split(splitQuote).map(Number)
     return nums.reduce((sum, num) => sum + num, 0);
 }
