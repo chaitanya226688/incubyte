4ff2b1eb8dc7ca20d93ff1c56e1b27d3daf9ad94
Venkata Chaithanya <chaitanya.evolutyz@gmail.com>
2025-02-22 14:14:54 +0530

    Test Case 2 Passed

--- src/Add.js
+++ src/Add.js
@@ -1,5 +1,8 @@
-const Add = () => {
-    return 0;
+const Add = (numbers) => {
+    if (numbers == "0") return 0;
+
+    let nums = numbers.split(" ").map(Number)
+    return nums.reduce((sum, num) => sum + num, 0);
 }
 
 export default Add
