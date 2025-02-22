e3402025acd9b10f04d255e5f55c8a0fdd694f8a
Venkata Chaithanya <chaitanya.evolutyz@gmail.com>
2025-02-22 14:18:31 +0530

    Test case 4 passed

--- src/Add.test.js
+++ src/Add.test.js
@@ -11,3 +11,7 @@ test("returns the number itself if only one number is provided", () => {
 test("add two numbers seperated by comma", () => {
     expect(Add("1,2")).toBe(3);
 });
+
+test("add numbers seperated by comma and new line", () => {
+    expect(Add("1,2\n3")).toBe(6);
+});
