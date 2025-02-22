b49ecd146dea3dff4a9962261ea5fa8d9987565a
Venkata Chaithanya <chaitanya.evolutyz@gmail.com>
2025-02-22 14:53:13 +0530

    Big Delimeter Pass

--- src/Add.js
+++ src/Add.js
@@ -4,15 +4,20 @@ const Add = (numbers) => {
     let splitQuote = /,|\n/
 
     if (numbers.startsWith("//")) {
+        const splitQuotesMatches = numbers.match(/^\/\/(\[.*?\])+\n/)
         const items = numbers.split("\n");
-        splitQuote = new RegExp(items[0][2])
+        if (splitQuotesMatches) {
+            splitQuote = splitQuotesMatches[1].replace("[", "").replace(']', "")
+            console.log(splitQuote)
+        } else {
+            splitQuote = new RegExp(items[0][2])
+        }
         numbers = items[1]
     }
 
-    let nums = numbers.split(splitQuote).map(Number)
+    let nums = numbers.split(splitQuote).map(Number).filter(element => element < 1001)
 
     const negaiveNumbers = nums.filter(element => element < 0)
-    console.log("negaiveNumbersn", negaiveNumbers)
     if (negaiveNumbers.length > 0) throw new Error('Negatives not allowed: ' + negaiveNumbers.join(", "))
 
     return nums.reduce((sum, num) => sum + num, 0);
--- src/Add.test.js
+++ src/Add.test.js
@@ -23,3 +23,11 @@ test("support new delimiter addition", () => {
 test("negative numbers will give error", () => {
     expect(() => Add("1,-2,-3,4")).toThrow("Negatives not allowed: -2, -3");
 });
+
+test("numbers bigger than 1000 should be ignored", () => {
+    expect(Add("2,1001")).toBe(2);
+});
+
+test("delimiters can be of any length", () => {
+    expect(Add("//[***]\n1***2***3")).toBe(6);
+});
